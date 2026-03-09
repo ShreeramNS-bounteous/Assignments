4523a82417acdcdb6c495ac7107f19573226648e
Shreeram N S <shreeram.ns@bounteous.com>
2026-03-09 15:54:32 +0530

    Processed using Single Thread

--- Day 1/src/Main.java
+++ Day 1/src/Main.java
@@ -4,19 +4,35 @@ import java.util.List;
 
 class Main{
     static void main() {
-//        FileGenerator.generateFiles();
+        System.out.println("Processing files using SINGLE thread...\n");
+        long startTime = System.currentTimeMillis();
 
-//        List<File> files = FileScanner.getFiles();
-//
-//        for (File file: files){
-//            System.out.println(file.getName());
-//        }
+        List<File> files = FileScanner.getFiles();
 
-       File file = new File("files/file1.txt");
-       FileResult result = FileProcessor.processFile(file);
+        int totalLines  = 0;
+        int totalWords = 0;
 
-        System.out.println("File: " + result.getFileName());
-        System.out.println("Line Count: " + result.getLineCount());
-        System.out.println("Word Count: " + result.getWordCount());
+        for (File file : files){
+            FileResult result = FileProcessor.processFile(file);
+
+            System.out.println("File: " + result.getFileName());
+            System.out.println("Line Count: " + result.getLineCount());
+            System.out.println("Word Count: " + result.getWordCount());
+
+            totalLines+= result.getLineCount();
+            totalWords+= result.getWordCount();
+        }
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("----------------------------------");
+        System.out.println("Summary");
+        System.out.println("----------------------------------");
+
+        System.out.println("Total Files Processed: " + files.size());
+        System.out.println("Total Lines: " + totalLines);
+        System.out.println("Total Words: " + totalWords);
+
+        System.out.println("Execution Time: " + (endTime - startTime) + " ms");
     }
 }
