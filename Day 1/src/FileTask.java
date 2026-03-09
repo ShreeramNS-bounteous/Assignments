50ef2d68789b21b96304e27f5c95b69df8e0cbe6
Shreeram N S <shreeram.ns@bounteous.com>
2026-03-09 16:02:01 +0530

    Created task by implementing Callable Interface

--- Day 1/src/FileTask.java
+++ Day 1/src/FileTask.java
@@ -1,2 +1,21 @@
-public class FileTask {
+import java.io.File;
+import java.util.concurrent.Callable;
+
+public class FileTask implements Callable<FileResult> {
+
+    private File file;
+
+    public FileTask(File file){
+        this.file = file;
+    }
+
+    @Override
+    public  FileResult call(){
+        try{
+            return FileProcessor.processFile(file);
+        }catch (Exception e){
+            System.out.println("Error processing file: "+ file.getName());
+            return new FileResult(file.getName(), 0,0);
+        }
+    }
 }
